49900c85a99c68302c8a6734061a7cc999f4ae5d
Daniel Armengod Sorrosal <danielarmengodsorrosal@gmail.com>
2019-10-31 17:17:38 +0100

    Subida de la preparación para la escucha de las llamadas

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //hellow world
+const express = require('express');
+const request = require('request');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/getVideos', (req, res) => {
+    console.log('The server has been launched');
+    console.log('----------------------------');
+    console.log('---         JAI          ---');
+    console.log('----------------------------');
+    res.send('{mierda}');
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}...`);
+});
